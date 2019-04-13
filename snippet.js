920661c96991a66abc33d65b472af1438e5c6725
subbes <subbes@gmail.com>
2019-04-12 20:25:11 -0700

    tried to fix scrolling
    ...but gave up

--- snippet.js
+++ snippet.js
@@ -1,13 +1,22 @@
 function offscreen(element){
-   var elem = document.getElementById(element);
-   return( elem.offsetTop > window.innerHeight || (elem.offsetHeight + elem.offSetTop) < 0 );
+   var elem = document.getElementsByClassName(element);
+   [].forEach.call(elem, function(el, i) {
+   if ( elem.offsetTop > window.innerHeight || (elem.offsetHeight + elem.offSetTop) < 0 )
+      return true;
+   }); // check if all of the load more buttons are offscreen, if even one is, scroll
+   
+   return false;
 } // check whether element is outside the viewport
 
 function scroll()
 {
-   if(offscreen("copyright_text")){
+   if(offscreen("load-more-container")){
       window.scrollTo(0,document.body.scrollHeight);  // scroll
-   }  // if the footer is outside the viewport, scroll
+      var elem = document.getElementsByClassName("load-more-container");
+         [].forEach.call(elem, function(el, i) {
+            el.click();
+         }); // click all the 'load more' buttons
+   }  // if the footer is outside the viewport, scroll and hit the 'load more' button
   setTimeout(afterscroll, 3000); // give the page 3 seconds to load
 }
 
